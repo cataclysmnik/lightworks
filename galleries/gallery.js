85b83325dcaf394ccb6fea42051fc7cb2894f024
cataclysmnik <sagniksinghaishere@gmail.com>
2024-11-19 09:30:52 +0530

    Polished Scroll to Top button

--- galleries/gallery.js
+++ galleries/gallery.js
@@ -6,9 +6,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
+    mybutton.classList.add("show");
   } else {
-    mybutton.style.display = "none";
+    mybutton.classList.remove("show");
   }
 }
 
