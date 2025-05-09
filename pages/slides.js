c36c39913b0b034754ab8d2b92b2da75161a09f2
cataclysmnik <sagniksinghaishere@gmail.com>
2025-05-09 11:52:11 +0530

    Created an WIP Explore page with placeholders

---
+++ pages/slides.js
@@ -0,0 +1,29 @@
+const slides = [
+    {
+        tag: "Website",
+        marquee: "Eclipse Interactive Art Portfolio",
+        image: "../assets/images/img16.jpg",
+    },
+    {
+        tag: "Branding",
+        marquee: "Solaris Avant-Garde Brand Identity",
+        image: "../assets/images/img16.jpg",
+    },
+    {
+        tag: "Experience",
+        marquee: "Nova Immersive Light Exhibition",
+        image: "../assets/images/img16.jpg",
+    },
+    {
+        tag: "Website",
+        marquee: "Luminex Virtual Reality Showcase",
+        image: "../assets/images/img16.jpg",
+    },
+    {
+        tag: "Marketing",
+        marquee: "Orion Digital Art Launch Campaign",
+        image: "../assets/images/img16.jpg",
+    },
+];
+
+export default slides;
